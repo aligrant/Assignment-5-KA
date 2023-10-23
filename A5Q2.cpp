fdb719b8a3c23c94fbd590d698eebf8ad2846735
aligrant <alessandrad@lindenschool.ca>
2023-10-23 13:37:28 -0400

    Update A5Q2.cpp

---
+++ A5Q2.cpp
@@ -0,0 +1,15 @@
+/*
+A landscaping company has a crew of co-op students who do both paving and (sometimes) fencing. The company
+charges $ 19.50 for paving each square metre, or any part of a square metre, (i.e. 2.1 becomes 3 sq. m) and $27.00 per
+metre, or any part of a metre, for fencing. (Hint: There is a C++ function called ceil that rounds towards +8.) All fences
+also have a $150.00 gate. Added to each bill is a $45.00 administration fee. HST is 13%.
+The sales representative for the company has created a file called “Sep_jobs2023.txt” that contains the jobs completed
+by the work crew for the month of September. Each line of the file contains the following information:
+? Job number
+? Fencing Job? (indicating whether fencing has been done as well as paving)
+? Shape (indicating the shape of the area paved/fenced)
+? Dimensions (varying number of distances depending on the shape)
+*/
+
+
+
