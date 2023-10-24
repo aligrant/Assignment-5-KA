546f7b538c3899e93b15fa7f3f49ce4d11a08d8f
Alessandra <77739460+aligrant@users.noreply.github.com>
2023-10-24 11:32:34 -0400

    Fixed degrees

--- A5Q3.cpp
+++ A5Q3.cpp
@@ -14,7 +14,7 @@ int mins_2_mid(int time) // function to get time in minutes
 {
 	int hours = time/100;
 	int minutes = time % 100; // to get minutes from midnight
-	int minutes_midnight = (hours*6) + minutes;	
+	int minutes_midnight = (hours*6) + minutes;
 	return minutes_midnight;
 }
 //Distance
@@ -26,33 +26,24 @@ float getDistance (float x, float y)
 //Angle
 float getBearingAngle (float x, float y) // get angle bearing using arctan and convert to degrees
 {
-	float angleRad = atan(y/x);
-	if (angleRad > M_PI ) // ensure that the value is between -pi < x < pi, if it isnt then it adds or subtracts pi accordingly
-	{
-		angleRad = angleRad - M_PI;
-	}
-	if (angleRad < -M_PI)
-	{
-		angleRad = angleRad + M_PI;
-	}
-	else
-	{
-		angleRad = angleRad;
-	}
+	//atan2 because its between pi and -pi
+	//pos x axis is 0 degrees
+	float angleRad = atan2(y,x);
+	//convert to degrees
 	float angleDeg = (angleRad *180)/ M_PI;
-	return angleDeg;	
+	return angleDeg;
 }
 
 int main()
 {
 	ifstream fileIn ("wascally_wabbits.txt"); // check file open/close
 	ofstream fileOut ("output.txt");
-	
+
 	if (!fileIn || !fileOut)
-	{
-		cout << "Unable to open files\n";
-		return EXIT_FAILURE;
-	}
+		{
+			cout << "Unable to open files\n";
+			return EXIT_FAILURE;
+		}
 	float time = 0.0;
 	float x_cord = 0.0;
 	float y_cord = 0.0;
@@ -62,16 +53,18 @@ int main()
 	float new_time=0;
 	fileOut<<setw(15)<<"MinTillMidnight"<<setw(11)<<"Distance"<<setw(10)<<"Angle\n";
 	while (fileIn >> time >> x_cord >> y_cord) // loop to make sure the code runs to the end of the file
-	{
-		time_rabbit_2=time;
-		new_time=max(time_rabbit_2,time_rabbit_1)-min(time_rabbit_2,time_rabbit_1);
-		if(shortest_time>new_time){
-			shortest_time=new_time;
+		{
+			//Get shortest time between rabbits
+			time_rabbit_2=time;
+			new_time=max(time_rabbit_2,time_rabbit_1)-min(time_rabbit_2,time_rabbit_1);
+			if(shortest_time>new_time)
+				{
+					shortest_time=new_time;
+				}
+			time_rabbit_1=time_rabbit_2;
+			fileOut << setw(15)<<setprecision(4)<<mins_2_mid(time)<<setw(11)<<getDistance(x_cord, y_cord);
+			fileOut << setw(9)<<getBearingAngle(x_cord, y_cord) << "\n"; //file output
 		}
-		time_rabbit_1=time_rabbit_2;
-		fileOut << setw(15)<<setprecision(4)<<mins_2_mid(time)<<setw(11)<<getDistance(x_cord, y_cord);
-		fileOut << setw(9)<<getBearingAngle(x_cord, y_cord) << "\n"; //file output
-	}
 	fileOut<<"The shortest time between rabbits is "<<shortest_time<<" minutes.";
 	fileIn.close();
 	fileOut.close(); // closing both output and input files
