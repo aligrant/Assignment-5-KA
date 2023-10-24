a8704a736dfb05c309e3fdf14198a26db4855573
Alessandra <77739460+aligrant@users.noreply.github.com>
2023-10-24 11:09:59 -0400

    Finished Q3

--- A5Q3.cpp
+++ A5Q3.cpp
@@ -1,36 +1,6 @@
-/* Intro
-Emily is tired of rabbits eating her lettuce plants in her backyard. She is thinking of designing a ballistic Nerf dart rabbit
-deterrent system, to be placed in the centre of her back lawn, which can be considered as (0,0). As part of the design
-phase, over a 24-hour period starting at midnight, she recorded the time a rabbit was spotted and the (x,y) coordinates in
-meters of the rabbit, saving them to a file called wascally_wabbits.txt. She wants to know the distance and heading
-(an angle from -180 to 180 degrees, where 0 degrees is along the positive x-axis) of each rabbit, and the shortest time in
-minutes between rabbit sightings.
-*/
-
-/* File Format
-wascally_wabbits.txt contains an unknown number of lines. Each line contains three values: a time, an x
-coordinate, and a y coordinate.
-? The time is an integer in an HHMM format on a 24 hour clock. For example, 1450 represents the time 2:50 pm.
-? The x and y coordinates are relative to a point at the centre of the lawn (0,0).
-? File data can be assumed to be valid coordinates, which will not include the point (0,0)
-*/
-
-/* The program:
-Write a function that:
-? Receives a time as an integer in HHMM format (eg. 0350)
-? Returns the time in minutes from midnight (eg. 230)
-Write a function that:
-? Receives a location x and y in m
-? Returns the distance to that location in m
-Write a function that:
-? Receives a location x and y in m
-? Returns the heading in degrees, between -180 and 180
-Write a program that:
-? Opens the file wascally_wabbits.txt and checks if it has opened
-? Opens an output file to store the results
-? Reads the data on each rabbit, and outputs to the file the distance and heading to each rabbit
-? Outputs to the file the shortest time, in minutes, between rabbits
-? Closes the files
+/*
+Alessandra Grant and Koustav Bhasin
+Version 1.0
 */
 
 #include <iostream>
@@ -39,7 +9,7 @@ Write a program that:
 #include <cmath>
 #include <iomanip>
 using namespace std;
-
+//Minutes till midnight
 int mins_2_mid(int time) // function to get time in minutes
 {
 	int hours = time/100;
@@ -47,13 +17,13 @@ int mins_2_mid(int time) // function to get time in minutes
 	int minutes_midnight = (hours*6) + minutes;	
 	return minutes_midnight;
 }
-
+//Distance
 float getDistance (float x, float y)
 {
 	float distance = sqrt(pow(x,2) + pow(y,2)); // use pyathagoras theorem to find the distance from 0,0
 	return distance;
 }
-
+//Angle
 float getBearingAngle (float x, float y) // get angle bearing using arctan and convert to degrees
 {
 	float angleRad = atan(y/x);
@@ -72,18 +42,8 @@ float getBearingAngle (float x, float y) // get angle bearing using arctan and c
 	float angleDeg = (angleRad *180)/ M_PI;
 	return angleDeg;	
 }
-/*float shortestTime(int time, int time1) (still a work in progress)
-{
-	if (time > time1)
-	{
-		int temp; 
-		temp = time;
-		time = time1;
-		time1 = temp;
-	}
-}*/
-int main()
 
+int main()
 {
 	ifstream fileIn ("wascally_wabbits.txt"); // check file open/close
 	ofstream fileOut ("output.txt");
@@ -96,15 +56,31 @@ int main()
 	float time = 0.0;
 	float x_cord = 0.0;
 	float y_cord = 0.0;
-	file 
+	float time_rabbit_1=0;
+	float time_rabbit_2=0;
+	float shortest_time=100000;//assuming this is greater than the first time between rabbits
+	float new_time=0;
+	fileOut<<setw(15)<<"MinTillMidnight"<<setw(11)<<"Distance"<<setw(10)<<"Angle\n";
 	while (fileIn >> time >> x_cord >> y_cord) // loop to make sure the code runs to the end of the file
 	{
-		fileOut << mins_2_mid(time) << "  " <<getDistance(x_cord, y_cord) << "  "<< getBearingAngle(x_cord, y_cord) << "\n"; //file output
-		cout << mins_2_mid(time) << "  " <<getDistance(x_cord, y_cord) << "  "<< getBearingAngle(x_cord, y_cord) << "\n"; //output on code to see whether it works
-		
+		time_rabbit_2=time;
+		new_time=max(time_rabbit_2,time_rabbit_1)-min(time_rabbit_2,time_rabbit_1);
+		if(shortest_time>new_time){
+			shortest_time=new_time;
+		}
+		time_rabbit_1=time_rabbit_2;
+		fileOut << setw(15)<<setprecision(4)<<mins_2_mid(time)<<setw(11)<<getDistance(x_cord, y_cord);
+		fileOut << setw(9)<<getBearingAngle(x_cord, y_cord) << "\n"; //file output
 	}
-	
+	fileOut<<"The shortest time between rabbits is "<<shortest_time<<" minutes.";
 	fileIn.close();
 	fileOut.close(); // closing both output and input files
 	return EXIT_SUCCESS;
 }
+//In the closed console:
+/*
+
+--------------------------------
+Process exited after 0.09827 seconds with return value 0
+Press any key to continue . . .
+*/
